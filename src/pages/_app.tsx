b3b3ca1151d5b5bf4a24b6c9e2bb75ac1ed6abe8
Eriktech <erik.lopez.tech@gmail.com>
2022-09-15 22:52:43 -0400

    exporting myApp correctly using wrapperStore vs withRedux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import '../styles/globals.css'
 
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
-
-import {store, wrapper} from '../redux/store'
+import {wrapper} from '../redux/store'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {store, props} = wrapper.useWrappedStore(pageProps)
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Component {...props.pageProps} />
     </Provider>
   )
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
